ad6b5bd4b2995e7349668895fa11db8f0f5ef0a3
Alex Belke <alex.belke.de@gmail.com>
2020-04-10 11:48:52 +0200

    cancel and complete orders

--- src/main/java/com/alexbelke/springbootdemo/util/OrderModelAssembler.java
+++ src/main/java/com/alexbelke/springbootdemo/util/OrderModelAssembler.java
@@ -1,14 +1,36 @@
 package com.alexbelke.springbootdemo.util;
 
 import com.alexbelke.springbootdemo.model.Order;
+import com.alexbelke.springbootdemo.model.Status;
+import com.alexbelke.springbootdemo.web.OrderController;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.server.RepresentationModelAssembler;
 import org.springframework.stereotype.Component;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
 @Component
 public class OrderModelAssembler implements RepresentationModelAssembler<Order, EntityModel<Order>> {
 	@Override
-	public EntityModel<Order> toModel(Order entity) {
-		return null;
+	public EntityModel<Order> toModel(Order order) {
+		// Unconditional links to single-item resource and aggregate root
+
+		EntityModel<Order> orderModel = new EntityModel<>(order,
+				linkTo(methodOn(OrderController.class).one(order.getId())).withSelfRel(),
+				linkTo(methodOn(OrderController.class).all()).withRel("orders")
+		);
+
+		// Conditional links based on state of the order
+
+		if (order.getStatus() == Status.IN_PROGRESS) {
+			orderModel.add(
+					linkTo(methodOn(OrderController.class)
+							.cancel(order.getId())).withRel("cancel"));
+			orderModel.add(
+					linkTo(methodOn(OrderController.class)
+							.complete(order.getId())).withRel("complete"));
+		}
+
+		return orderModel;
 	}
 }
--- src/main/java/com/alexbelke/springbootdemo/web/OrderController.java
+++ src/main/java/com/alexbelke/springbootdemo/web/OrderController.java
@@ -7,6 +7,9 @@ import com.alexbelke.springbootdemo.util.OrderModelAssembler;
 import com.alexbelke.springbootdemo.util.exceptions.OrderNotFoundException;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.RepresentationModel;
+import org.springframework.hateoas.mediatype.vnderrors.VndErrors;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,12 +20,12 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
-class OrderController {
+public class OrderController {
 
 	private final OrderRepository orderRepository;
 	private final OrderModelAssembler assembler;
 
-	OrderController(OrderRepository orderRepository,
+	public OrderController(OrderRepository orderRepository,
 					OrderModelAssembler assembler) {
 
 		this.orderRepository = orderRepository;
@@ -30,7 +33,7 @@ class OrderController {
 	}
 
 	@GetMapping("/orders")
-	CollectionModel<EntityModel<Order>> all() {
+	public CollectionModel<EntityModel<Order>> all() {
 
 		List<EntityModel<Order>> orders = orderRepository.findAll().stream()
 				.map(assembler::toModel)
@@ -41,7 +44,7 @@ class OrderController {
 	}
 
 	@GetMapping("/orders/{id}")
-	EntityModel<Order> one(@PathVariable Long id) {
+	public EntityModel<Order> one(@PathVariable Long id) {
 		Order order = orderRepository.findById(id)
 				.orElseThrow(() -> new OrderNotFoundException(id));
 
@@ -49,7 +52,7 @@ class OrderController {
 	}
 
 	@PostMapping("/orders")
-	ResponseEntity<EntityModel<Order>> newOrder(@RequestBody Order order) {
+	public ResponseEntity<EntityModel<Order>> newOrder(@RequestBody Order order) {
 
 		order.setStatus(Status.IN_PROGRESS);
 		Order newOrder = orderRepository.save(order);
@@ -57,5 +60,35 @@ class OrderController {
 		return ResponseEntity
 				.created(linkTo(methodOn(OrderController.class).one(newOrder.getId())).toUri())
 				.body(assembler.toModel(newOrder));
+	}
+
+	@DeleteMapping("/orders/{id}/cancel")
+	public ResponseEntity<RepresentationModel> cancel(@PathVariable Long id) {
+
+		Order order = orderRepository.findById(id).orElseThrow(() -> new OrderNotFoundException(id));
+
+		if (order.getStatus() == Status.IN_PROGRESS) {
+			order.setStatus(Status.CANCELLED);
+			return ResponseEntity.ok(assembler.toModel(orderRepository.save(order)));
+		}
+
+		return ResponseEntity
+				.status(HttpStatus.METHOD_NOT_ALLOWED)
+				.body(new VndErrors.VndError("Method not allowed", "You can't cancel an order that is in the " + order.getStatus() + " status"));
+	}
+
+	@PutMapping("/orders/{id}/complete")
+	public ResponseEntity<RepresentationModel> complete(@PathVariable Long id) {
+
+		Order order = orderRepository.findById(id).orElseThrow(() -> new OrderNotFoundException(id));
+
+		if (order.getStatus() == Status.IN_PROGRESS) {
+			order.setStatus(Status.COMPLETED);
+			return ResponseEntity.ok(assembler.toModel(orderRepository.save(order)));
+		}
+
+		return ResponseEntity
+				.status(HttpStatus.METHOD_NOT_ALLOWED)
+				.body(new VndErrors.VndError("Method not allowed", "You can't complete an order that is in the " + order.getStatus() + " status"));
 	}
 }
